efa2861dd9b6ab46bc4d1d525f54d771625fcdff
Oren Ohayon <mailorenn@gmail.com>
2024-03-06 10:59:00 +0200

    main modified to call browser with e2e functions

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from app import start_play
+from Tools.e2e import main_function
 import webbrowser
 
 start_play()
-
-webbrowser.open('http://localhost:5040')
+main_function()
+# webbrowser.open('http://localhost:5040')
 
